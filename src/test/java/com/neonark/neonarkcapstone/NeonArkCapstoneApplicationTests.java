2a7819b07ac64153fde78c2302506b3347e1ea16
ncanales12 <nicolas.canales@g.austincc.edu>
2026-05-05 21:13:18 -0400

    initial spring boot setup working, server runs without database

---
+++ src/test/java/com/neonark/neonarkcapstone/NeonArkCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neonark.neonarkcapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeonArkCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
