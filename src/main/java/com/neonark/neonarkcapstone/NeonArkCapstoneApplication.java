64955c4d1e73c9ca2aab0ea9f7f03a4cd714a31f
ncanales12 <nicolas.canales@g.austincc.edu>
2026-05-05 21:21:38 -0400

    connected spring boot to postgres and verified db connection

--- src/main/java/com/neonark/neonarkcapstone/NeonArkCapstoneApplication.java
+++ src/main/java/com/neonark/neonarkcapstone/NeonArkCapstoneApplication.java
@@ -3,11 +3,7 @@ package com.neonark.neonarkcapstone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// turning off database for now so app can start clean
-@SpringBootApplication(excludeName = {
-        "org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration",
-        "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration"
-})
+@SpringBootApplication
 public class NeonArkCapstoneApplication {
 
     public static void main(String[] args) {
