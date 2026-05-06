a7a1ffcffe5749180db53420d8c76228c967c679
ncanales12 <nicolas.canales@g.austincc.edu>
2026-05-05 21:43:59 -0400

    added HabitatRepository and verified multi-entity setup

---
+++ src/main/java/com/neonark/neonarkcapstone/repository/HabitatRepository.java
@@ -0,0 +1,7 @@
+package com.neonark.neonarkcapstone.repository;
+
+import com.neonark.neonarkcapstone.entity.Habitat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HabitatRepository extends JpaRepository<Habitat, Long> {
+}
